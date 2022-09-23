9ea201c5ee344671ae821cf9e2e9cf697191d3c6
rra <rra@emachines.local>
2022-09-23 20:19:33 +0300

    Button component importation removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button, notification } from 'antd';
+import { notification } from 'antd';
 import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
