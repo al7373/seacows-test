2d12007f9794728d597c39d638a42c2b834e30f9
rra <rra@emachines.local>
2022-09-24 07:59:08 +0300

    done -> 1.link to metamask wallet, get balance if current address account and display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,101 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { notification } from 'antd';
 import styles from '../styles/Home.module.scss'
+import { useEffect, useRef, useState } from 'react'
+import detectEthereumProvider from '@metamask/detect-provider';
+import { notification } from 'antd';
+
+const showNot = (msg, key='error') => {
+  notification.open({
+    message: '',
+    className: `${ styles.mynot }`,
+    duration: null,
+    description:
+      <div className={ styles.description }>
+        <i></i>
+        <div>
+          <p>{ msg }</p>
+        </div>
+      </div>,
+    onClick: () => {
+      console.log('Notification Clicked!');
+    },
+    key
+  });
+}
 
 const Home: NextPage = () => {
 
-  const showNot = () => {
-    notification.open({
-      message: '',
-      className: `${ styles.mynot }`,
-      duration: null,
-      description:
-        <div className={ styles.description }>
-          <i></i>
-          <div>
-            <p>ddddd</p>
-          </div>
-        </div>,
-      onClick: () => {
-        console.log('Notification Clicked!');
-      },
-    });
+  const accountRef = useRef(null)
+	const [balance, setBalance] = useState(0);
+
+	async function handleAccountsChanged(accounts) {
+		if (accounts.length === 0) {
+			showNot('Please connect to MetaMask.');
+		} else if (accounts[0] !== accountRef.current) {
+			accountRef.current = accounts[0];
+			console.log("accountRef.current", accountRef.current)
+			const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+			handleChainChanged(chainId);
+
+			const balanceResult = await window.ethereum.request({
+				method: 'eth_getBalance',
+				params: [accountRef.current, 'latest']
+			})
+			
+			let wei = parseInt(balanceResult);
+			let balance = wei/(10**18);
+
+			setBalance(balance);
+
+		}
+	}
+
+	function handleChainChanged(_chainId) {
+		console.log('_chainId', _chainId)
+	}
+
+
+
+  function handleConnect(){
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(handleAccountsChanged)
+			.catch((err) => {
+				if (err.code === 4001) {
+					showNot('Please connect to MetaMask.');
+				} else {
+					console.error(err);
+				}
+			});
   }
+
+	useEffect(function(){
+
+		(async function(){
+			const provider = await detectEthereumProvider();
+			if(provider){
+
+        if (provider !== window.ethereum) {
+          showNot('Do you have multiple wallets installed?');
+        }
+
+				window.ethereum
+					.request({ method: 'eth_accounts' })
+					.then(handleAccountsChanged)
+
+				window.ethereum.on('chainChanged', handleChainChanged);
+
+			} else {
+				showNot('Please install MetaMask.');
+			}
+		})();
+
+
+	}, [])
+
   return (
     <div className={styles.container}>
-      <button onClick={showNot}>dialog</button>
+      <button onClick={ handleConnect }>link to metamask wallet</button>
+			<div>Balance: <strong>{ balance } ETH</strong></div>
     </div>
   )
 }
