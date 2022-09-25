db172912f07fabdf9af419a5f95fe48d89409a16
rra <rra@emachines.local>
2022-09-25 19:17:13 +0300

    components/DisplayPair

---
+++ components/DisplayPair/DisplayPair.js
@@ -0,0 +1,5 @@
+
+export default function DisplayPair(){
+  return <div>pair</div>
+}
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,9 @@ import txDecoder from 'ethereum-tx-decoder';
 import axios from 'axios';
 import erc20ABI from 'erc-20-abi';
 
+import DisplayPair from '../components/DisplayPair/DisplayPair';
+import objectHash from 'object-hash';
+
 const showNot = (msg, key='error') => {
   notification.open({
     message: '',
@@ -37,6 +40,104 @@ const Home: NextPage = () => {
 	const seacowsPairContractRef = useRef(null)
 
 	const [balance, setBalance] = useState(0);
+  const [readingNft, setReadingNft] = useState(false);
+  const [nfts, setNfts] = useState([]);
+
+  async function readNft(){
+
+      let seacowsPairContract = new web3Ref.current.eth.Contract(
+        SeacowsPairABI,
+        '0x1c9f47f8c42c3a8be36dcbe3d49e365b8099c7df'
+      );
+
+      let factory = await seacowsPairContract.methods.factory().call()
+
+      console.log("factory", factory)
+
+      const baseURL = '/api/my-etherscan'
+
+      let { data: { result } } = await axios.get(
+        baseURL, {
+        params: {
+          module: 'account',
+          action: 'txlist',
+          address: factory,
+          startblock: 0,
+          endblock: 99999999,
+          page: 1,
+          offset: 20,
+          sort: 'desc',
+        }
+      })
+
+      let { data: { result: factoryABI } } = await axios.get(
+        baseURL, {
+        params: {
+          module: 'contract',
+          action: 'getabi',
+          address: factory,
+        }
+      })
+
+      factoryABI = JSON.parse(factoryABI)
+    
+      let fnDecoder = new txDecoder.FunctionDecoder(factoryABI);
+
+      result = result.filter(({functionName, isError}) => 
+        (functionName.startsWith('createPairERC20') || functionName.startsWith('createPairETH')) && 
+        isError === "0"
+      )
+
+      let endResults = [];
+
+      for(let i = 0; i < result.length; i++){
+
+        let example = result[i];
+        let decodedInput = fnDecoder.decodeFn(example.input);
+
+        if(example.functionName.startsWith('createPairERC20')){
+
+          const token = new web3Ref.current.eth.Contract(erc20ABI, decodedInput.params.token)
+
+          endResults.push({
+            "key": objectHash(decodedInput),
+            "initialNftIds": decodedInput.params.initialNFTIDs,
+            "nft": decodedInput.params.nft,
+            "correspondingToken": {
+              "symbol": await token.methods.symbol().call(),
+              "name": await token.methods.name().call(),
+            }
+          })
+
+        }
+
+        if(example.functionName.startsWith('createPairETH')){
+
+          endResults.push({
+            "key": objectHash(decodedInput),
+            'initialNftIds': decodedInput._initialNFTIDs,
+            'nft': decodedInput._nft,
+            "correspondingToken": {
+              'symbol': 'ETH',
+              'name': 'Ethereum',
+            }
+          })
+
+        }
+
+
+      }
+
+      return endResults;
+  }
+
+  function handleReadNft(){
+     setReadingNft(true);
+     readNft().then((data) => {
+       setNfts(data);
+       setReadingNft(false);
+     })
+  }
 
 	async function handleAccountsChanged(accounts) {
 		if (accounts.length === 0) {
@@ -55,66 +156,8 @@ const Home: NextPage = () => {
 			let wei = parseInt(balanceResult);
 			let balance = wei/(10**18);
 
+
 			setBalance(balance);
-
-      seacowsPairContractRef.current = new web3Ref.current.eth.Contract(
-        SeacowsPairABI,
-        '0x1c9f47f8c42c3a8be36dcbe3d49e365b8099c7df'
-      );
-
-      let factory = await seacowsPairContractRef.current.methods.factory().call()
-
-      console.log("factory", factory)
-
-
-      const baseURL = '/api/my-etherscan'
-
-      let { data: { result } } = await axios.get(
-        baseURL, {
-        params: {
-          module: 'account',
-          action: 'txlist',
-          address: factory,
-          startblock: 0,
-          endblock: 99999999,
-          page: 1,
-          offset: 20,
-          sort: 'desc',
-        }
-      })
-
-      //tokony sarahana ny traitement-n'ny PAIRERC20 sy PAIRETH
-      result = result.filter(({functionName, isError}) => 
-        (functionName.startsWith('createPairERC20') || functionName.startsWith('createPairETH')) && 
-        isError === "0"
-      )
-
-      let example = result[0];
-
-      let { data: { result: factoryABI } } = await axios.get(
-        baseURL, {
-        params: {
-          module: 'contract',
-          action: 'getabi',
-          address: factory,
-        }
-      })
-
-      factoryABI = JSON.parse(factoryABI)
-
-      let fnDecoder = new txDecoder.FunctionDecoder(factoryABI);
-
-      let decodedInput = fnDecoder.decodeFn(example.input);
-
-      //ho an'ny ERC20 ihany vao misy ny params
-      console.log(decodedInput);
-
-      //console.log(decodedInput.params.nft);
-      const token = new web3Ref.current.eth.Contract(erc20ABI, decodedInput.params.token)
-       
-      console.log("symbol", await token.methods.symbol().call()) // prints the token's symbol
-      console.log("name", await token.methods.name().call()) // prints the token's name
-
 
 		}
 	}
@@ -155,7 +198,6 @@ const Home: NextPage = () => {
 
 				window.ethereum.on('chainChanged', handleChainChanged);
 
-
 			} else {
 				showNot('Please install MetaMask.');
 			}
@@ -168,7 +210,18 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <button onClick={ handleConnect }>1.link to metamask wallet</button>
 			<div>Balance: <strong>{ balance } ETH</strong></div>
-      <button onClick={ handleConnect }>2.Read nft from the pool</button>
+      <button onClick={ handleReadNft } disabled={ readingNft }>
+        { readingNft ? "loading ..." : "2.Read nft from the pool" }
+      </button>
+      { nfts.length > 0 && 
+        <ul>
+        {nfts.map((data) =>
+          <li key={ data.key }>
+            <DisplayPair data={ data }/>
+          </li>
+        )} 
+        </ul>
+      }
     </div>
   )
 }
